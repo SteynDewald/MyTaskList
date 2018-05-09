4e6669a6ec449f68d7ecb0eac731e9cbf54b61a7
Dewald Steyn <Dewald Steyn>
2018-05-09 11:04:28 +0200

    Add project files.

---
+++ TODO assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TODO_assignment.MvcApplication" Language="C#" %>
---
+++ TODO assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TODO_assignment.Startup))]
+namespace TODO_assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
